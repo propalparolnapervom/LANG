b6f6d004c50dd6b05c860d6b093a681a2ac39e34
Sergii Burtovyi <37516081+propalparolnapervom@users.noreply.github.com>
2022-02-08 18:50:49 +0200

    Create 01_strings_functions.py

---
+++ PYTHON/Examples/01_strings_functions.py
@@ -0,0 +1,22 @@
+# Print string
+print('What is your name?')
+
+# Request input, remember it as a variable
+myName = input()
+
+# Get lenght of variable, remember it as a variable
+lengthOfTheName = len(myName)
+
+# Print string, use variable
+print('Ok, so the name is ' + myName + ' and its lenght is ' + str(lengthOfTheName) + ' symbols')
+
+
+
+# Print string
+print('What is your age?')
+
+# Request input, remember it as a variable
+myAge = input()
+
+# Print string, use ariphmetic
+print('You will be ' + str(int(myAge) + 5) + ' in 5 years')
